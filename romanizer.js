3c68c99e09a861ad468b962ea0e61f3c362ba280
Claudio Scafesi <scafesi@gmail.com>
2018-02-12 11:39:51 +0100

    romanizer old tests

--- romanizer.js
+++ romanizer.js
@@ -1,4 +1,47 @@
+var translator = [
+  {arabic: 10, roman: 'X'},
+  {arabic: 9, roman: 'IX'},
+  {arabic: 5, roman: 'V'},
+  {arabic: 4, roman: 'IV'},
+  {arabic: 1, roman: 'I'},
+];
+
 function romanizer(number) {
+
+  var roman = '';
+
+translator.forEach(function(translate){
+  while (number >= translate.arabic){
+    roman += translate.roman;
+    number -= translate.arabic;
+  }
+});
+
+//   while (number >0){
+//
+//
+//   if (number >= 10)  {
+//         roman += 'X';
+//         number -= 10;
+//     }
+//
+//   else  if (number >= 9)  {
+//       roman += 'IX';
+//       number -= 9;
+//   }
+//   else if (number >= 5)  {
+//     roman += 'V';
+//     number -= 5;
+//   }else if (number >= 4){
+//     roman += 'IV';
+//     number -= 4;
+//   }else if (number >= 1){
+//     roman += 'I';
+//     number -= 1;
+//   }
+// }
+return roman;
+
 }
 
 module.exports = romanizer;
--- tests/romanizer.test.js
+++ tests/romanizer.test.js
@@ -1,7 +1,24 @@
 const test = require('tape');
 const romanizer = require('../romanizer.js');
+const translator = require('../romanizer.js').translator;
 
 test('arabic => roman', function(t) {
   t.equals(romanizer(1), 'I', '1 => I');
+  t.equals(romanizer(2), 'II', '2 => II');
+  t.equals(romanizer(4), 'IV', '4 => IV');
+  t.equals(romanizer(5), 'V', '5 => V');
+  t.equals(romanizer(6), 'VI', '6 => VI');
+  t.equals(romanizer(7), 'VII', '7 => VII');
+  t.equals(romanizer(8), 'VIII', '8 => VIII');
+  t.equals(romanizer(9), 'IX', '9 => IX');
+  t.equals(romanizer(10), 'X', '10 => X');
+  t.equals(romanizer(12), 'XII', '12 => XII');
+  t.equals(romanizer(25), 'XXV', '25 => XXV');
+
+
+
+
+
+
   t.end();
 });
